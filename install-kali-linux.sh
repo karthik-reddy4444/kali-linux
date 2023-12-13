c70a93960d1d910ea0ea47ffa9ba53eafd4ac5ae
M.Karthik Reddy <maramkarthik21@gmail.com>
2023-12-13 11:18:47 +0530

    Create install-kali-linux.sh

---
+++ install-kali-linux.sh
@@ -0,0 +1,11 @@
+apt-get update -y
+echo " "
+apt-get upgrade -y
+echo " "
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+echo " "
+chmod +x *
+echo " "
+./install-nethunter-termux
+echo " "
+nh
